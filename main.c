390039ceb762c4bbcfaff722e509b23ee8bf587b
Kevencruz <75061582+Kevencruz@users.noreply.github.com>
2025-12-09 16:50:46 -0600

    Update main.c

--- main.c
+++ main.c
@@ -620,37 +620,6 @@ void SystemClock_Config(void)
   }
 }
 
-/*static void MX_I2C1_Init(void)
-{
-  hi2c1.Instance             = I2C1;
-  hi2c1.Init.ClockSpeed      = 100000;
-  hi2c1.Init.DutyCycle       = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1     = 0;
-  hi2c1.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2     = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
-    Error_Handler();
-  }
-} */
-
-/*static void MX_USART2_UART_Init(void)
-{
-  huart2.Instance          = USART2;
-  huart2.Init.BaudRate     = 115200;
-  huart2.Init.WordLength   = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits     = UART_STOPBITS_1;
-  huart2.Init.Parity       = UART_PARITY_NONE;
-  huart2.Init.Mode         = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK) {
-    Error_Handler();
-  }
-}*/
-
 static void MX_GPIO_Init(void)
 {
   __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -664,3 +633,4 @@ void Error_Handler(void)
     // Trap here
   }
 }
+
